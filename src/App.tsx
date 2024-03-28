9d4935debcd5e431331765a96887f5e2d0668d55
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-03-28 15:08:29 +0100

    feat: implement mobile navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 // import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [navbarVisible, setNavbarVisible] = useState<boolean>(false);
   //   const [prevScrollPos, setPrevScrollPos] = useState<number>(0);
   //   const [navbarVisible, setNavbarVisible] = useState<boolean>(true);
 
@@ -37,23 +39,79 @@ function App() {
     <div className="w-full h-full flex flex-col pt-[66px] md:pt-[80px]">
       <nav
         id="navbar"
-        className={`transition-all px-[18px] md:px-[130px] z-40 py-[18px] shadow-2xl shadow-blue-900/20 flex items-center justify-between w-full top-[0px] left-0 fixed bg-white`}
+        className={`px-[40px] md:px-[130px] z-40 py-[18px] shadow-2xl shadow-blue-900/20 flex items-center justify-between w-full top-[0px] left-0 fixed bg-white`}
       >
         <h1 className="text-[20px] md:text-[30px] md:font-bold font-medium text-[#333333]">
           Surfnet
         </h1>
 
-        <svg
-          className="ast-mobile-svg ast-menu-svg"
-          fill="currentColor"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
+        <div className="md:flex items-center justify-center gap-14 hidden">
+          <p className="text-[16px] leading-[24px] font-medium text-[#333333] cursor-pointer">
+            Home
+          </p>
+          <p className="text-[16px] leading-[24px] font-medium text-[#333333] cursor-pointer">
+            About
+          </p>
+          <p className="text-[16px] leading-[24px] font-medium text-[#333333] cursor-pointer">
+            Blog
+          </p>
+          <p className="text-[16px] leading-[24px] font-medium text-[#333333] cursor-pointer">
+            Billing
+          </p>
+          <p className="text-[16px] leading-[24px] font-medium text-[#333333] cursor-pointer">
+            Blog
+          </p>
+        </div>
+
+        {navbarVisible ? (
+          <svg
+            className="md:hidden cursor-pointer w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 50 50"
+            onClick={() => setNavbarVisible(!navbarVisible)}
+          >
+            <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
+          </svg>
+        ) : (
+          <svg
+            className="md:hidden cursor-pointer"
+            fill="currentColor"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            onClick={() => setNavbarVisible(!navbarVisible)}
+          >
+            <path d="M3 13h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 7h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 19h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1z"></path>
+          </svg>
+        )}
+
+        <div
+          className={`pb-[5px] transition duration-300 ease-in-out ${
+            navbarVisible ? "top-[66px]" : "-top-[300px]"
+          } shadow-2xl shadow-blue-900/20 flex flex-col items-center justify-between w-full left-0 absolute bg-white px-10 md:hidden`}
         >
-          <path d="M3 13h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 7h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1zM3 19h18c0.552 0 1-0.448 1-1s-0.448-1-1-1h-18c-0.552 0-1 0.448-1 1s0.448 1 1 1z"></path>
-        </svg>
+          <p className="text-[16px] leading-[24px] font-normal text-[#333333] cursor-pointer text-left w-full border-b py-3 hover:text-green-500 transition-colors">
+            Home
+          </p>
+          <p className="text-[16px] leading-[24px] font-normal text-[#333333] cursor-pointer text-left w-full border-b py-3 hover:text-green-500 transition-colors">
+            About
+          </p>
+          <p className="text-[16px] leading-[24px] font-normal text-[#333333] cursor-pointer text-left w-full border-b py-3 hover:text-green-500 transition-colors">
+            Blog
+          </p>
+          <p className="text-[16px] leading-[24px] font-normal text-[#333333] cursor-pointer text-left w-full border-b py-3 hover:text-green-500 transition-colors">
+            Billing
+          </p>
+          <p className="text-[16px] leading-[24px] font-normal text-[#333333] cursor-pointer text-left w-full py-3 hover:text-green-500 transition-colors">
+            Blog
+          </p>
+        </div>
       </nav>
 
       <section className="relative border h-[500px] md:h-[700px]">
@@ -151,7 +209,7 @@ function App() {
         </div>
       </section>
 
-      <section className="relative p-10 md:px-[130px] md:py-[65px] flex md:flex-row flex-col gap-12 items-start w-full justify-center">
+      <section className="relative p-10 md:px-[130px] md:py-[65px] flex md:flex-row flex-col gap-12 items-center md:items-start w-full justify-center">
         <img
           src="https://seolounge.radiantthemes.com/wp-content/uploads/2020/12/home-17-brand-left.png"
           alt=""
@@ -216,7 +274,9 @@ function App() {
       <div className="w-full bg-blue-950 flex flex-col p-10 md:p-20 gap-10 md:px-[130px]">
         <div className="flex flex-col md:flex-row w-full items-center md:justify-between gap-5">
           <h3 className="text-white font-medium text-center md:text-left text-[22px] leading-[35.2px] md:w-[800px]">
-          We’re on a mission to build a better future where technology creates good jobs for everyone. Fusce sed rutrum risus pulvinar tortor et. Aenean suscipit ege.
+            We’re on a mission to build a better future where technology creates
+            good jobs for everyone. Fusce sed rutrum risus pulvinar tortor et.
+            Aenean suscipit ege.
           </h3>
 
           <button className="text-base font-medium text-white bg-green-500 rounded-md md:rounded-md px-7 md:px-12 py-4 md:py-5 hover:bg-green-600 flex items-center justify-center gap-3 max-w-max">
@@ -242,7 +302,9 @@ function App() {
           </p>
         </div>
 
-        <p className="text-[12px] leading-[24px] text-center text-gray-400">© 2024 Surfnet Limited. All Rights Reserved Apollo</p>
+        <p className="text-[12px] leading-[24px] text-center text-gray-400">
+          © 2024 Surfnet Limited. All Rights Reserved Apollo
+        </p>
       </div>
     </div>
   );
